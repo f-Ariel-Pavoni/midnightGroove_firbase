62c1341a7cbb151bd63820481ac721f172ac2d44
Ariel Pavoni <flvariel@gmail.com>
2026-07-27 23:17:04 -0300

    Integración inicial con Firebase

--- src/services/discoService.js
+++ src/services/discoService.js
@@ -1,9 +1,25 @@
-export const getDiscos = async () => {
-  const respuesta = await fetch(`${import.meta.env.BASE_URL}data/discos.json`);
-  if (!respuesta.ok) throw new Error("Error al cargar discos.");
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/config";
 
-  return respuesta.json();
+export const getDiscos = async () => {
+  try {
+    const snapshot = await getDocs(collection(db, "discos"));
+    return snapshot.docs.map((doc) => ({
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("ERROR FIREBASE:", error);
+    throw new Error("Error al cargar discos desde Firestore.");
+  }
 };
+
+//  funcion que toma discos del local
+//  export const getDiscos = async () => {
+//   const respuesta = await fetch(`${import.meta.env.BASE_URL}data/discos.json`);
+//   if (!respuesta.ok) throw new Error("Error al cargar discos.");
+
+//   return respuesta.json();
+// };
 
 export const getDiscoById = async (id) => {
   const discos = await getDiscos();
